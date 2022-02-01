e35315e7e6d371eb1e20314f4f22436ab7aca127
davidlippai <lippai28@gmail.com>
2022-02-01 19:58:02 +0100

    mobilV

--- app.js
+++ app.js
@@ -3,3 +3,13 @@ function menuOnClick() {
   document.getElementById("nav").classList.toggle("change");
   document.getElementById("menu-bg").classList.toggle("change-bg");
 }
+
+// SPEISE Btn Function 
+
+let foodMenuBtn = document.getElementById("foodMenu");
+
+foodMenuBtn.addEventListener('click', function(){
+  let foodListDisplay = document.getElementById("foodList");
+
+  foodListDisplay.classList.toggle("displayNoneFood");
+});
