354483b31502e14298208b25b480148f44cedc90
davidlippai <lippai28@gmail.com>
2022-04-06 19:22:24 +0200

    effect

--- app.js
+++ app.js
@@ -34,3 +34,28 @@ suppenBtn.addEventListener('click',function(){
   suppenListDisplay.classList.toggle("displayNoneFood");
 
 })
+
+//--------------------Fade in Effect
+
+const faders = document.querySelectorAll('.fade-in')
+const appearOptions = {
+  threshold: 1,
+  //rootMargin: "0px 0px 0px 0px"
+};
+
+const appearOnScroll = new IntersectionObserver 
+(function(entries, appearOnScroll){
+  entries.forEach(entry => {
+    if (!entry.isIntersecting){
+      return;
+    } else {
+      entry.target.classList.add('appear');
+      appearOnScroll.unobserve(entry.target)
+    }
+  })
+}, 
+appearOptions)
+
+faders.forEach(fader => {
+  appearOnScroll.observe(fader);
+})
